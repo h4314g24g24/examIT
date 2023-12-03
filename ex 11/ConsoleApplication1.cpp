34a88c35e59f4f256df741261939e88125bb28c2
h4314g24g24 <m.igonchenkov@gmail.com>
2023-12-03 20:51:24 +0200

    first commit

---
+++ ex 11/ConsoleApplication1.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    
+    const int ряди = 22;
+    const int колона = 2;
+
+
+
+    int masiv[ряди][колона];
+
+   
+    cout << "елементи масиву:" << endl;
+    for (int i = 0; i < ряди; ++i) {
+        for (int j = 0; j < колона; ++j) {
+            cin >> masiv[i][j];
+        }
+    }
+
+    for (int i = 0; i < ряди; ++i) {
+        for (int j = 0; j < колона; ++j) {
+            cout << masiv[i][j] << " ";
+        }
+            cout << endl;
+        
+    return 0;
+}
+        //Відсортувати масив -
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
